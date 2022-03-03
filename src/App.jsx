ea3fe797446e74208a9fbca5050758892aa8c607
Maximiliano Martinez <martinezmassera@gmail.com>
2022-03-03 15:29:16 -0300

    Clase 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
-import NavBar from './component/NavBar/NavBar';
 import ItemListContainer from './component/contenedor/ItemListContainer';
+import NavBar from './component/NavBar/NavBar';
+
 
 
 function App() {
   return (
     <div className="App">
         <NavBar />
-        <ItemListContainer initial = {1} stock = {10}/>
+        <ItemListContainer />
     </div>
   );
 }
