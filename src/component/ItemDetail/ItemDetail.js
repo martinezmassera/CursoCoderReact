26a5189d5d4a31f73f489ac1b1547bc123f90719
martinezmassera <martinezmassera@gmail.com>
2022-03-10 15:15:26 -0300

    Clase 7

--- src/component/ItemDetail/ItemDetail.js
+++ src/component/ItemDetail/ItemDetail.js
@@ -1,14 +1,15 @@
 import ItemCount from "../contenedor/ItemCount"
+
 const ItemDetail = ({product}) => {
   return(
-<div className="row">
+<div className="row detalleProducto">
 <div className="col">
     <div className="container">
         <h3>{product.title}</h3>
-        <img src={product.pictureUrl} alt='Foto'/>
+        <img className="img-responsive" src={product.pictureUrl} alt='Foto'/>
     </div>
 </div>
-<div className="col">
+<div className="col textoDetalle">
     <div className="container">
     <p>{product.detalle}</p>
 <ItemCount initial = {1} stock = {product.stock}/>
