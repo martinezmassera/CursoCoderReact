3a91ad4f54f1f574ca5fb402f0f6b186d273ee70
Maximiliano Martinez <martinezmassera@gmail.com>
2022-03-21 23:35:53 -0300

    clase 10

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,25 +1,48 @@
+import { Button } from "react-bootstrap"
+import { Link } from "react-router-dom"
 import { useCartContext } from "../../context/cartContext"
+import './cart.css'
 
 const Cart = () => {
-    const { cartList } = useCartContext()
-    console.log(cartList)
-    return (
-        <div>
-            {cartList.map(item =>
-                <div class="col-md-8" key={item.id}>
-                    <div class="cart-item">
-                        <div class="cart-item-imagen">
-                            <img className="img-responsive" src={item.pictureUrl} alt='Foto' />
+    const { cartList, limpiarCart, clearProd } = useCartContext()
+    
+
+return (
+    <div>
+        <div className="container">
+            <div className="row">
+
+                <div className="col-md-8" >
+                    {cartList.map(item =>
+                        <div className="cart-item" key={item.id}>
+                            <div className="cart-item-imagen">
+                                <img className="img-responsive" src={item.pictureUrl} alt='Foto' />
+                            </div>
+                            <div className="cart-item-detalle">
+                                <p>{item.title}</p>
+                            </div>
+                            <div className="cart-item-cant">
+                                <p>$ {item.cantidad}</p>
+                            </div>
+                            <div className="cart-item-clear">
+                            <Button variant="danger" onClick={() => clearProd(item.id)}>x</Button>
+                            </div>
                         </div>
-                        <div class="cart-item-detalle">
-                            <p>{item.title}</p>
-                        </div>
-                    </div>
+
+                    )
+                    }
                 </div>
-            )
-            }
-</div>
-    )
+                <div className="col-md-4">
+                {cartList.length === 0 ? <Link to='/'>
+        <Button variant="success">Volver a Comprar</Button></Link>
+              :
+               <Button variant="danger" onClick={limpiarCart}>VaciarCarrto</Button>
+             }
+                </div>
+            </div>
+        </div>
+    </div>
+)
 }
 
 export default Cart
--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -4,17 +4,35 @@ const cartContext = createContext();
 
 export const useCartContext = () => useContext(cartContext)
 
-function CartContextProvider({children}) {
-    const[cartList, setCartList] = useState([])
+function CartContextProvider({ children }) {
+    const [cartList, setCartList] = useState([])
 
-    const agregarAlCarrito = (item) =>{
-        setCartList([ ...cartList, item])
+    const agregarAlCarrito = (item) => {
+        const index = cartList.map(prod => prod.id).indexOf(item.id)
+        if (index === -1) {
+            setCartList([...cartList, item])
+        } else {
+            const cant = cartList[index].cantidad
+            cartList[index].cantidad = item.cantidad + cant 
+            const newCartList = [ ...cartList ]
+            setCartList(newCartList)
+        }
+    }
+    const limpiarCart=()=>{
+        setCartList([])
     }
 
-    return(
+    const clearProd = (id) => { 
+        setCartList( cartList.filter( prod => prod.id !== id ) )
+    }
+
+
+    return (
         <cartContext.Provider value={{
             cartList,
-            agregarAlCarrito
+            agregarAlCarrito,
+            limpiarCart,
+            clearProd
         }}>
             {children}
         </cartContext.Provider>
